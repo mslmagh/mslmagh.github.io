0afff622b871b80bcdd5278077e6ca44ae1ecfaa
Muslum <muslumagah7@gmail.com>
2025-02-15 19:56:21 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mesajınız gönderildi!');
+    // Burada form verilerini işleyebilir veya bir API'ye gönderebilirsiniz.
+});
